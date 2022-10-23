6d508909af3e5f14d318313070eff9c3c13704e7
saifulalamfahime <you@example.com>
2022-10-23 17:48:10 +0600

    cart total amount count

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,9 +2,26 @@ import React from 'react';
 import "./Cart.css";
 
 const Cart = (props) => {
+    const { cart } = props;
+
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tex = total * 0.1;
+    const grandTotal = total + shipping + tex;
+
     return (
         <div className='cart'>
-             <h4>Orders Summery {props.cart.length}</h4>
+             <h4>Order Summary</h4>
+             <p>Selected Items: {cart.length}</p>
+             <p>Total price: ${total} </p>
+             <p>Total Shipping: ${shipping}</p>
+             <p>Tax: ${tex}</p>
+             <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
